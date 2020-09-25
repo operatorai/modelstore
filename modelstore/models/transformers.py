57d59dd1add0dcbe2f53c712842acb1073a8e668
nlathia <neal.lathia@gmail.com>
2020-09-25 22:31:57 +0100

    Add support for transformers

---
+++ modelstore/models/transformers.py
@@ -0,0 +1,79 @@
+#    Copyright 2020 Neal Lathia
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import os
+from functools import partial
+
+from modelstore.models.modelmanager import ModelManager
+
+# pylint disable=import-outside-toplevel
+
+
+class TransformersManager(ModelManager):
+
+    """
+    Model persistence for Transformer models:
+    https://huggingface.co/transformers/main_classes/model.html#transformers.TFPreTrainedModel.save_pretrained
+    https://github.com/huggingface/transformers/blob/e50a931c118b9f55f77a743bf703f436bf7a7c29/src/transformers/modeling_utils.py#L676
+
+    """
+
+    @classmethod
+    def required_dependencies(cls) -> list:
+        return ["transformers"]
+
+    @classmethod
+    def optional_dependencies(cls) -> list:
+        """ Returns a list of dependencies that, if installed
+        are useful to log info about """
+        deps = super().optional_dependencies()
+        return deps + ["torch", "tensorflow"]
+
+    def _required_kwargs(self):
+        return ["model", "tokenizer"]
+
+    def _get_functions(self, **kwargs) -> list:
+        return [
+            partial(
+                _save_transformers,
+                config=kwargs["config"],
+                model=kwargs["model"],
+                tokenizer=kwargs["tokenizer"],
+            ),
+        ]
+
+
+def _save_transformers(
+    tmp_dir: str,
+    config: "transformers.PretrainedConfig",
+    model: "transformers.PreTrainedModel",
+    token: "transformers.PreTrainedTokenizerBase",
+) -> str:
+    import transformers
+
+    if config and not isinstance(config, transformers.PretrainedConfig):
+        raise TypeError("Config is not a transformers.PretrainedConfig!")
+    if not isinstance(model, transformers.PreTrainedModel):
+        raise TypeError("Model is not a transformers.PreTrainedModel!")
+    if token and not isinstance(token, transformers.PreTrainedTokenizerBase):
+        raise TypeError(
+            "Tokenizer is not a transformers.PreTrainedTokenizerBase!"
+        )
+
+    model_dir = os.path.join(tmp_dir, "transformers")
+    if config is not None:
+        config.save_pretrained(model_dir)
+    model.save_pretrained(model_dir)
+    token.save_pretrained(model_dir)
+    return model_dir
---
+++ tests/models/test_transformers.py
@@ -0,0 +1,91 @@
+#    Copyright 2020 Neal Lathia
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import json
+import os
+
+import pytest
+from transformers import (
+    AutoConfig,
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    DistilBertForSequenceClassification,
+    DistilBertTokenizer,
+)
+from transformers.file_utils import CONFIG_NAME
+
+from modelstore.models.transformers import (
+    TransformersManager,
+    _save_transformers,
+)
+
+# pylint: disable=protected-access
+# pylint: disable=redefined-outer-name
+
+
+@pytest.fixture
+def model_name():
+    return "distilbert-base-cased"
+
+
+@pytest.fixture
+def model_config(model_name):
+    return AutoConfig.from_pretrained(
+        model_name, num_labels=3, finetuning_task="mnli",
+    )
+
+
+@pytest.fixture
+def tokenizer(model_name):
+    return AutoTokenizer.from_pretrained(model_name)
+
+
+@pytest.fixture()
+def model(model_name, model_config):
+    return AutoModelForSequenceClassification.from_pretrained(
+        model_name, config=model_config,
+    )
+
+
+def test_required_kwargs():
+    mngr = TransformersManager()
+    assert mngr._required_kwargs() == ["model", "tokenizer"]
+
+
+def test_get_functions():
+    mngr = TransformersManager()
+    assert len(mngr._get_functions(config="c", model="m", tokenizer="t")) == 1
+
+
+def test_save_transformers(model_config, model, tokenizer, tmp_path):
+    exp = os.path.join(tmp_path, "transformers")
+    file_path = _save_transformers(tmp_path, model_config, model, tokenizer)
+    assert exp == file_path
+
+    # Validate config
+    config_file = os.path.join(exp, CONFIG_NAME)
+    assert os.path.exists(config_file)
+    with open(config_file, "r") as lines:
+        config_json = json.loads(lines.read())
+    assert config_json == json.loads(model_config.to_json_string())
+
+    # Validate model
+    model = AutoModelForSequenceClassification.from_pretrained(
+        file_path, config=model_config,
+    )
+    assert isinstance(model, DistilBertForSequenceClassification)
+
+    # Validate tokenizer
+    token = AutoTokenizer.from_pretrained(file_path)
+    assert isinstance(token, DistilBertTokenizer)
