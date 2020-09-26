67995becbb69b800a03bb032b6da59e51e35f5d2
nlathia <neal.lathia@gmail.com>
2020-09-26 18:02:45 +0100

    Format ModuleNotFound error

--- modelstore/models/missingmanager.py
+++ modelstore/models/missingmanager.py
@@ -43,4 +43,4 @@ class MissingDepManager(ModelManager):
     def create_archive(self, **kwargs) -> str:
         logger.error("Error: %s is not installed", self.library)
         logger.error("Please install it and try again")
-        raise ModuleNotFoundError("Library is not installed")
+        raise ModuleNotFoundError(f"{self.library} is not installed")
