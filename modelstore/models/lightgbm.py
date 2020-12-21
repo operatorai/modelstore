ad449a34e0b7a28baf3da79cd00b0f0cd6613907
nlathia <neal.lathia@gmail.com>
2020-12-21 16:23:48 +0000

    Add lightgbm manager

---
+++ modelstore/models/lightgbm.py
@@ -0,0 +1,67 @@
+#    Copyright 2020 Neal Lathia
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import os
+from functools import partial
+
+from modelstore.models.common import save_json
+from modelstore.models.modelmanager import ModelManager
+from modelstore.utils.log import logger
+
+MODEL_JSON = "model.json"
+
+
+class LightGbmManager(ModelManager):
+
+    """
+    Model persistence for light gbm models:
+    https://lightgbm.readthedocs.io/en/latest/Python-Intro.html#training
+    """
+
+    @classmethod
+    def required_dependencies(cls) -> list:
+        return ["lightgbm"]
+
+    def _required_kwargs(self):
+        return ["model"]
+
+    def model_info(self, **kwargs) -> dict:
+        """ Returns meta-data about the model's type """
+        return {"library": "lightgbm"}
+
+    def _get_functions(self, **kwargs) -> list:
+        return [
+            partial(dump_model, model=kwargs["model"]),
+        ]
+
+    def _get_params(self, **kwargs) -> dict:
+        """
+        Returns a dictionary containing any model parameters
+        that are available
+        """
+        return {}
+
+
+def dump_model(tmp_dir: str, model: "lgb.Booster") -> str:
+    """From the docs: dump model into JSON file"""
+    import lightgbm as lgb
+
+    if not isinstance(model, lgb.Booster):
+        raise TypeError("Model is not a lgb.Booster!")
+
+    logger.debug("Dumping lightgbm model")
+    model_file = os.path.join(tmp_dir, MODEL_JSON)
+    with open(model_file, "w") as out:
+        out.write(model.dump_model())
+    return model_file
