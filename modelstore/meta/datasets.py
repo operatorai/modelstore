2c243062c9ae1587ed573e22edf7e24959767ff7
nlathia <neal.lathia@gmail.com>
2020-12-23 14:22:52 +0000

    Add dataset type functions

--- modelstore/meta/datasets.py
+++ modelstore/meta/datasets.py
@@ -17,31 +17,45 @@ except ImportError:
     PANDAS_EXISTS = False
 
 
+def is_numpy_array(values):
+    if NUMPY_EXISTS:
+        return isinstance(values, np.ndarray)
+    return False
+
+
+def is_pandas_dataframe(values):
+    if PANDAS_EXISTS:
+        return isinstance(values, pd.DataFrame)
+    return False
+
+
+def is_pandas_series(values):
+    if PANDAS_EXISTS:
+        return isinstance(values, pd.Series)
+    return False
+
+
 def describe_training(dataset) -> Optional[dict]:
     """ Returns a description of a dataset"""
-    if NUMPY_EXISTS:
-        if isinstance(dataset, np.ndarray):
-            return {"shape": list(dataset.shape)}
-    if PANDAS_EXISTS:
-        if isinstance(dataset, pd.DataFrame):
-            return {"shape": list(dataset.shape)}
+    if is_numpy_array(dataset):
+        return {"shape": list(dataset.shape)}
+    if is_pandas_dataframe(dataset):
+        return {"shape": list(dataset.shape)}
     logger.debug(f"Trying to describe unknown type: {type(dataset)}")
     return None
 
 
 def describe_labels(labels) -> Optional[dict]:
-    if NUMPY_EXISTS:
-        if isinstance(labels, np.ndarray):
-            unique, counts = np.unique(labels, return_counts=True)
-            return {
-                "shape": list(labels.shape),
-                "values": dict(zip(unique, counts)),
-            }
-    if PANDAS_EXISTS:
-        if isinstance(labels, pd.Series):
-            return {
-                "shape": list(labels.shape),
-                "values": labels.value_counts().to_dict(),
-            }
+    if is_numpy_array(labels):
+        unique, counts = np.unique(labels, return_counts=True)
+        return {
+            "shape": list(labels.shape),
+            "values": dict(zip(unique, counts)),
+        }
+    if is_pandas_series(labels):
+        return {
+            "shape": list(labels.shape),
+            "values": labels.value_counts().to_dict(),
+        }
     logger.debug(f"Trying to describe unknown type: {type(labels)}")
     return None
