41df7e533c09602eba7aad1ec5926d0e63b56d86
nlathia <neal.lathia@gmail.com>
2020-12-31 00:26:14 +0000

    Implement uploads in hosted storage

--- modelstore/storage/hosted.py
+++ modelstore/storage/hosted.py
@@ -11,8 +11,14 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
+import json
+import os
+
+import requests
 from modelstore.storage.storage import CloudStorage
 from modelstore.utils.log import logger
+from tqdm import tqdm
+from tqdm.utils import CallbackIOWrapper
 
 # pylint: disable=protected-access
 
@@ -22,8 +28,7 @@ _URL_ROOT = "https://europe-west1-revival-287212.cloudfunctions.net/"
 class HostedStorage(CloudStorage):
 
     """
-    OperatorStorage is a managed model store. No dependencies required.
-
+    HostedStorage is a managed model store. No dependencies required.
     Usage of this storage requires you to have an `api_key`.
     """
 
@@ -35,16 +40,46 @@ class HostedStorage(CloudStorage):
         """ No dependencies or setup required; validation returns True """
         return self.api_key is not None and len(self.api_key) > 0
 
-    # def get_url(self, )
+    def _get_url(self, domain: str, model_id: str, url_type: str) -> str:
+        """ Returns a pre-signed URL for up/downloading models """
+        endpoint = os.path.join(_URL_ROOT, "model-generate-url")
+        body = {
+            "api_key": self.api_key,
+            "domain": domain,
+            "model_id": model_id,
+            "url_type": url_type,
+        }
+        rsp = requests.post(endpoint, data=json.dumps(body))
+        if rsp.status_code == 200:
+            return rsp.json()["url"]
+        raise Exception(f"Error: {rsp.text.strip()}")
 
-    # def _push(self, source: str, destination: str):
-    # @TODO use curl to upload the model
-    # logger.debug("Uploading from: %s...", source)
+    def _upload(self, local_path, upload_url):
+        """ Uploads a local file to an upload URL, showing a progress bar """
+        file_path = os.path.abspath(local_path)
+        file_size = os.stat(file_path).st_size
+        with open(file_path, "rb") as f:
+            with tqdm(
+                total=file_size, unit="B", unit_scale=True, unit_divisor=1024
+            ) as t:
+                wrapped_file = CallbackIOWrapper(t.update, f, "read")
+                requests.put(upload_url, data=wrapped_file)
+
+    def _register_upload(self, domain: str, model_id: str):
+        endpoint = os.path.join(_URL_ROOT, "model-register-uploaded")
+        body = {
+            "domain": domain,
+            "model_id": model_id,
+            "api_key": self.api_key,
+        }
+        rsp = requests.post(endpoint, data=json.dumps(body))
+        if rsp.status_code != 200:
+            raise Exception(f"Error: {rsp.text.strip()}")
 
     def upload(self, domain: str, model_id: str, local_path: str) -> dict:
-        # @TODO request a presigned URL
-        # @TODO self._push()
-        # @TODO register upload
+        upload_url = self._get_url(domain, model_id, "PUT")
+        self._upload(local_path, upload_url)
+        self._register_upload(domain, model_id)
         return {
             "type": "operator:cloud-storage",
         }
