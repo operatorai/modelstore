64905042a57bb9c81e988f1cb4b7dd3cd0930a17
nlathia <neal.lathia@gmail.com>
2021-02-13 19:01:52 +0000

    Deal with earlier versions of metadata

--- modelstore/storage/util/versions.py
+++ modelstore/storage/util/versions.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 
 def sort_by_version(meta_data: dict):
-    version = meta_data["modelstore"]
+    version = meta_data.get("modelstore", "0.0.1")
     if version in ["0.0.4", "0.0.5"]:
         return datetime.strptime(
             meta_data["code"]["created"], "%Y/%m/%d/%H:%M:%S"
