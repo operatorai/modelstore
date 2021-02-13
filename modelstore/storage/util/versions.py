46aa844b81891078461fd923828280a7d8259fa3
nlathia <neal.lathia@gmail.com>
2021-02-13 18:41:41 +0000

    Add sorting by modelstore version back

--- modelstore/storage/util/versions.py
+++ modelstore/storage/util/versions.py
@@ -2,10 +2,13 @@ from datetime import datetime
 
 
 def sort_by_version(meta_data: dict):
-    created = datetime.strptime(
-        meta_data["meta"]["created"], "%Y/%m/%d/%H:%M:%S"
-    )
-    return created
+    version = meta_data["modelstore"]
+    if version in ["0.0.4", "0.0.5"]:
+        return datetime.strptime(
+            meta_data["code"]["created"], "%Y/%m/%d/%H:%M:%S"
+        )
+    # Earlier versions of modelstore had a different meta-data structure
+    return datetime.strptime(meta_data["meta"]["created"], "%Y/%m/%d/%H:%M:%S")
 
 
 def sorted_by_created(versions: list):
