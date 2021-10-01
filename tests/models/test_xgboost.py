4893116ab4495fd0d10ec42a3e45fb15045e0ae3
nlathia <neal.lathia@gmail.com>
2021-10-01 13:50:25 +0100

    Remove unused imports

--- tests/models/test_xgboost.py
+++ tests/models/test_xgboost.py
@@ -11,10 +11,8 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
-import math
 import os
 
-import numpy as np
 import pytest
 import xgboost as xgb
 from modelstore.models import xgboost
