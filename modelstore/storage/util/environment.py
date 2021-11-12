901df47a5211b52ab5e5f7aa72dcb4c3003c8a34
nlathia <neal.lathia@gmail.com>
2021-11-12 11:01:48 +0000

    Add comments to get_value()

--- modelstore/storage/util/environment.py
+++ modelstore/storage/util/environment.py
@@ -1,9 +1,23 @@
 import os
+from typing import Optional
 
 
-def get_value(arg: str, env_key: str, allow_missing: bool = False) -> str:
+def get_value(
+    arg: str, env_key: str, allow_missing: bool = False
+) -> Optional[str]:
+    """Modelstore storage can optionally be instantiated using
+    environment variables. This function is used to decide whether to
+    - pull a variable from the user's environment;
+    - return the one that was passed in;
+    - return None
+    """
     if arg is not None:
+        # arg has been passed in as non-None, so return it
         return arg
     if env_key not in os.environ and allow_missing:
+        # The environment key doesn't exist for a variable that
+        # is allowed to be missing, so return None
         return None
+    # Return the environment variable; this will KeyError if it
+    # is missing
     return os.environ[env_key]
