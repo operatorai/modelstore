6fdd0378b8ed63da6d39e34bb2158cdfd2f81684
nlathia <neal.lathia@gmail.com>
2021-11-15 22:27:27 +0000

    Add placeholder gcloud _remove() test

--- tests/storage/test_gcloud.py
+++ tests/storage/test_gcloud.py
@@ -59,7 +59,7 @@ def gcloud_client(gcloud_bucket):
 
 
 @pytest.fixture
-def gcloud_model_store(gcloud_client):
+def gcloud_storage(gcloud_client):
     return GoogleCloudStorage(
         project_name="", bucket_name=_MOCK_BUCKET_NAME, client=gcloud_client
     )
@@ -89,52 +89,63 @@ def test_create_fails_with_missing_environment_variables(monkeypatch):
         _ = GoogleCloudStorage()
 
 
-def test_validate_existing_bucket(gcloud_model_store):
-    assert gcloud_model_store.validate()
+def test_validate_existing_bucket(gcloud_storage):
+    assert gcloud_storage.validate()
 
 
 def test_validate_missing_bucket(gcloud_client):
-    gcloud_model_store = GoogleCloudStorage(
+    gcloud_storage = GoogleCloudStorage(
         project_name="", bucket_name="missing-bucket", client=gcloud_client
     )
-    assert not gcloud_model_store.validate()
+    assert not gcloud_storage.validate()
 
 
-def test_push(temp_file, remote_file_path, gcloud_model_store):
-    result = gcloud_model_store._push(temp_file, remote_file_path)
+def test_push(temp_file, remote_file_path, gcloud_storage):
+    result = gcloud_storage._push(temp_file, remote_file_path)
     assert result == remote_file_path
 
 
-def test_pull(temp_file, tmp_path, remote_file_path, gcloud_model_store):
+def test_pull(temp_file, tmp_path, remote_file_path, gcloud_storage):
     # Push the file to storage
-    remote_destination = gcloud_model_store._push(temp_file, remote_file_path)
+    remote_destination = gcloud_storage._push(temp_file, remote_file_path)
 
     # Pull the file back from storage
     local_destination = os.path.join(tmp_path, TEST_FILE_NAME)
-    result = gcloud_model_store._pull(remote_destination, tmp_path)
+    result = gcloud_storage._pull(remote_destination, tmp_path)
     assert result == local_destination
     assert os.path.exists(local_destination)
     assert file_contains_expected_contents(local_destination)
 
 
-def test_read_json_objects_ignores_non_json(gcloud_bucket, gcloud_model_store):
-    gcloud_model_store.client.list_blobs.return_value = [
+def test_remove(temp_file, remote_file_path, gcloud_storage):
+    # Push the file to storage
+    remote_destination = gcloud_storage._push(temp_file, remote_file_path)
+
+    # Remove the file
+    gcloud_storage._remove(remote_destination)
+
+    # Trying to read the file errors
+    # @TODO assert the file has been removed
+
+
+def test_read_json_objects_ignores_non_json(gcloud_bucket, gcloud_storage):
+    gcloud_storage.client.list_blobs.return_value = [
         Blob(name="test-file-source-1.txt", bucket=gcloud_bucket),
         Blob(name="test-file-source-2.txt", bucket=gcloud_bucket),
     ]
     # Argument (remote prefix) is ignored here because of mock above
-    items = gcloud_model_store._read_json_objects("")
+    items = gcloud_storage._read_json_objects("")
     assert len(items) == 0
 
 
-def test_read_json_object_fails_gracefully(gcloud_model_store):
+def test_read_json_object_fails_gracefully(gcloud_storage):
     # Read a file that does not contain any JSON
     # Argument (remote prefix) is ignored here because of mock above
-    item = gcloud_model_store._read_json_object("")
+    item = gcloud_storage._read_json_object("")
     assert item is None
 
 
-def test_storage_location(gcloud_model_store):
+def test_storage_location(gcloud_storage):
     # Asserts that the location meta data is correctly formatted
     prefix = "/path/to/file"
     exp = {
@@ -142,7 +153,7 @@ def test_storage_location(gcloud_model_store):
         "bucket": _MOCK_BUCKET_NAME,
         "prefix": prefix,
     }
-    assert gcloud_model_store._storage_location(prefix) == exp
+    assert gcloud_storage._storage_location(prefix) == exp
 
 
 @pytest.mark.parametrize(
@@ -166,10 +177,10 @@ def test_storage_location(gcloud_model_store):
         ),
     ],
 )
-def test_get_location(gcloud_model_store, meta_data, should_raise, result):
+def test_get_location(gcloud_storage, meta_data, should_raise, result):
     # Asserts that pulling the location out of meta data is correct
     if should_raise:
         with pytest.raises(ValueError):
-            gcloud_model_store._get_storage_location(meta_data)
+            gcloud_storage._get_storage_location(meta_data)
     else:
-        assert gcloud_model_store._get_storage_location(meta_data) == result
+        assert gcloud_storage._get_storage_location(meta_data) == result
