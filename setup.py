fa1ae23629630850ef7307b3ce9f65805486794e
nlathia <neal.lathia@gmail.com>
2021-11-26 12:19:40 +0000

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as lines:
 
 setup(
     name="modelstore",
-    version="0.0.71",
+    version="0.0.72",
     packages=find_packages(exclude=["tests", "examples", "docs"]),
     include_package_data=True,
     description="modelstore is a library for versioning, exporting, storing, and loading machine learning models",
