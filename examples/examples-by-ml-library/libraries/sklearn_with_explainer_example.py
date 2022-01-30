3f0f11b1f9c2042a431447a631d0cc6f449d85d1
Neal Lathia <neallathia@gmail.com>
2022-01-30 14:26:58 +0000

    Add example upload of both model+explainer

--- examples/examples-by-ml-library/libraries/sklearn_with_explainer_example.py
+++ examples/examples-by-ml-library/libraries/sklearn_with_explainer_example.py
@@ -3,11 +3,10 @@ from modelstore.model_store import ModelStore
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
 from libraries.util.datasets import load_diabetes_dataset
 from libraries.util.domains import DIABETES_DOMAIN
-
-EXPLAINER_DOMAIN = f"{DIABETES_DOMAIN}-explainer"
 
 
 def _train_example_model() -> Pipeline:
@@ -32,25 +31,32 @@ def _train_example_model() -> Pipeline:
     shap_values = explainer.shap_values(X_test)[0]
     print(f"🔍  Shap values={shap_values[:10]}.")
 
-    return explainer
+    return model, explainer
 
 
 def train_and_upload(modelstore: ModelStore) -> dict:
-    # Train a model and return the explainer
-    explainer = _train_example_model()
+    # Train a scikit-learn model and an explainer
+    model, explainer = _train_example_model()
 
-    # Upload the explainer to the model store
-    print(f'⤴️  Uploading the explainer to the "{EXPLAINER_DOMAIN}" domain.')
-    meta_data = modelstore.upload(EXPLAINER_DOMAIN, explainer=explainer)
+    # Upload the model to the model store
+    print(f'⤴️  Uploading the sklearn model to the "{DIABETES_DOMAIN}" domain.')
+    meta_data = modelstore.upload(DIABETES_DOMAIN, model=model, explainer=explainer)
     return meta_data
 
 
 def load_and_test(modelstore: ModelStore, model_id: str):
-    # Load the explainer back into memory!
-    print(f'⤵️  Loading the explainer "{EXPLAINER_DOMAIN}" domain model={model_id}')
-    explainer = modelstore.load(EXPLAINER_DOMAIN, model_id)
+    # Load the model back into memory!
+    print(
+        f'⤵️  Loading sklearn/shap modelsL domain="{DIABETES_DOMAIN}" model={model_id}'
+    )
+    models = modelstore.load(DIABETES_DOMAIN, model_id)
 
     # Run some example predictions
+    _, X_test, _, y_test = load_diabetes_dataset()
+    results = mean_squared_error(y_test, models["sklearn"].predict(X_test))
+    print(f"🔍  Loaded model MSE={results}.")
+
+    # Run some example explanations
     _, X_test, _, _ = load_diabetes_dataset()
-    shap_values = explainer.shap_values(X_test)[0]
+    shap_values = models["shap"].shap_values(X_test)[0]
     print(f"🔍  Shap values={shap_values[:10]}.")
