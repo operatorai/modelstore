2eea3d765f57d5e576193e264ba3d7ea27fc8466
Neal Lathia <neallathia@gmail.com>
2022-02-20 14:30:41 +0000

    Sort functions and add comments

--- modelstore/model_store.py
+++ modelstore/model_store.py
@@ -30,13 +30,6 @@ from modelstore.storage.storage import CloudStorage
 @dataclass(frozen=True)
 class ModelStore:
 
-    """
-    ModelStore provides an class to:
-    - Create archives containing trained models;
-    - Upload those archives into cloud storage;
-    - List and get info about the models you have stored in the cloud
-    """
-
     # The backend provider, e.g. "gcloud"
     storage: CloudStorage
 
@@ -111,14 +104,20 @@ class ModelStore:
             libraries.append(manager)
         object.__setattr__(self, "_libraries", libraries)
 
+    """
+    DOMAINS: a domain is a string that is used to group several models together
+    (e.g., belonging to the same end usage). Domains are created automatically
+    when a model is first uploaded into it.
+    """
+
     def list_domains(self) -> list:
         """Returns a list of dicts, containing info about all
         of the domains"""
         return self.storage.list_domains()
 
-    def get_model_info(self, domain: str, model_id: str) -> dict:
-        """Returns the meta-data for a given model"""
-        return self.storage.get_meta_data(domain, model_id)
+    """
+    MODELS: multiple models can be added to a domain; 
+    """
 
     def list_versions(self, domain: str, state_name: Optional[str] = None) -> list:
         warnings.warn(
@@ -133,37 +132,14 @@ class ModelStore:
         is given results are filtered to models set to that state"""
         return self.storage.list_models(domain, state_name)
 
-    def upload(self, domain: str, **kwargs) -> dict:
-        # Figure out which library the kwargs match with
-        managers = matching_managers(self._libraries, **kwargs)
-        if len(managers) == 1:
-            return managers[0].upload(domain, **kwargs)
-
-        # If we match on more than one manager (e.g., a model
-        # and an explainer)
-        manager = MultipleModelsManager(managers, self.storage)
-        return manager.upload(domain, **kwargs)
-
-    def load(self, domain: str, model_id: str):
-        meta_data = self.get_model_info(domain, model_id)
-        ml_library = meta_data["model"]["model_type"]["library"]
-        if ml_library == MultipleModelsManager.NAME:
-            manager = MultipleModelsManager([], self.storage)
-        else:
-            manager = get_manager(ml_library, self.storage)
-        with tempfile.TemporaryDirectory() as tmp_dir:
-            model_files = self.download(tmp_dir, domain, model_id)
-            return manager.load(model_files, meta_data)
-
-    def download(self, local_path: str, domain: str, model_id: str = None) -> str:
-        local_path = os.path.abspath(local_path)
-        archive_path = self.storage.download(local_path, domain, model_id)
-        with tarfile.open(archive_path, "r:gz") as tar:
-            tar.extractall(local_path)
-        os.remove(archive_path)
-        return local_path
+    """
+    MODEL STATES: a model state is a string that has a 1:many relationship
+    with models.
+    """
 
     def list_model_states(self) -> list:
+        """Returns a list of the available model states that
+        have been created with `create_model_state()`"""
         return self.storage.list_model_states()
 
     def create_model_state(self, state_name: str):
@@ -181,3 +157,47 @@ class ModelStore:
         This will not error if the model was never set to that state to begin
         with, but it will if that state does not exist"""
         return self.storage.unset_model_state(domain, model_id, state_name)
+
+    """
+    MODELS: a model archive is created and stored when using upload(). Each model
+    is given an id, which can be used to retrieve meta data about it. Models
+    can be downloaded as files, or loaded straight back into memory.
+    """
+
+    def get_model_info(self, domain: str, model_id: str) -> dict:
+        """Returns the meta-data for a given model"""
+        return self.storage.get_meta_data(domain, model_id)
+
+    def upload(self, domain: str, **kwargs) -> dict:
+        """Creates an archive for a model (from the kwargs), uploads it
+        to storage, and returns a dictionary of meta-data about the model"""
+        # Figure out which library the kwargs match with
+        managers = matching_managers(self._libraries, **kwargs)
+        if len(managers) == 1:
+            return managers[0].upload(domain, **kwargs)
+
+        # If we match on more than one manager (e.g., a model
+        # and an explainer)
+        manager = MultipleModelsManager(managers, self.storage)
+        return manager.upload(domain, **kwargs)
+
+    def load(self, domain: str, model_id: str):
+        """Loads a model into memory"""
+        meta_data = self.get_model_info(domain, model_id)
+        ml_library = meta_data["model"]["model_type"]["library"]
+        if ml_library == MultipleModelsManager.NAME:
+            manager = MultipleModelsManager([], self.storage)
+        else:
+            manager = get_manager(ml_library, self.storage)
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            model_files = self.download(tmp_dir, domain, model_id)
+            return manager.load(model_files, meta_data)
+
+    def download(self, local_path: str, domain: str, model_id: str = None) -> str:
+        """Downloads the model a domain to local_path"""
+        local_path = os.path.abspath(local_path)
+        archive_path = self.storage.download(local_path, domain, model_id)
+        with tarfile.open(archive_path, "r:gz") as tar:
+            tar.extractall(local_path)
+        os.remove(archive_path)
+        return local_path
