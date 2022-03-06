82c0b13c4f11ebb32f99fdafeb5f4f6408c61fb9
Neal Lathia <neallathia@gmail.com>
2022-03-06 00:08:38 +0000

    Set logging to info

--- modelstore/utils/log.py
+++ modelstore/utils/log.py
@@ -20,10 +20,10 @@ def get_logger():
     log = logging.getLogger(name="modelstore")
     formatter = logging.Formatter("%(asctime)s - %(message)s")
     handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.DEBUG)
+    handler.setLevel(logging.INFO)
     handler.setFormatter(formatter)
 
-    log.setLevel(logging.DEBUG)
+    log.setLevel(logging.INFO)
     log.addHandler(handler)
     return log
 
