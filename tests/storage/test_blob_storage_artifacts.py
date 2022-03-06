2cf966787adc843cf99368dca745a8cb727fb19c
Neal Lathia <neallathia@gmail.com>
2022-03-06 00:20:36 +0000

    Remove commented out code from test

--- tests/storage/test_blob_storage_artifacts.py
+++ tests/storage/test_blob_storage_artifacts.py
@@ -58,24 +58,4 @@ def test_download():
 
 
 def test_delete_model():
-    # prefix = self._get_storage_location(meta_data)
-    # if not skip_prompt:
-    #     message = f"Delete model from domain={domain} with model_id={model_id}?"
-    #     if not click.confirm(message):
-    #         logger.info("Aborting; not deleting model")
-
-    # # Delete the artifact itself
-    # self._remove(prefix)
-
-    # # Set the model as deleted in the meta data by unsetting it from
-    # # all custom states, setting it to a reserved state, and then deleting
-    # # the main meta-data file
-    # for state_name in self.list_model_states():
-    #     self.unset_model_state(domain, model_id, state_name)
-
-    # self.set_model_state(domain, model_id, ReservedModelStates.DELETED.value)
-
-    # logger.debug("Deleting meta-data for %s=%s", domain, model_id)
-    # remote_path = self._get_metadata_path(domain, model_id)
-    # self._remove(remote_path)
     pass
