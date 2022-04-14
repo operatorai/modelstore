720758c2ad4ad5ac8390571b7b13fa49687a4d03
Neal Lathia <neallathia@gmail.com>
2022-04-15 00:27:34 +0100

    Uncomment to run all

--- examples/examples-by-ml-library/run-all.sh
+++ examples/examples-by-ml-library/run-all.sh
@@ -1,5 +1,5 @@
 set -e
-backends=( filesystem ) # aws azure gcloud )
+backends=( filesystem aws azure gcloud )
 frameworks=( annoy catboost fastai file gensim keras lightgbm \
 	mxnet onnx-sklearn onnx-lightgbm prophet pytorch pytorch-lightning sklearn \
 	sklearn-with-explainer sklearn-with-extras skorch \
