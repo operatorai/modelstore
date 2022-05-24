28d3cc640215ffd64298c35f0b5a4ac584b4b3e3
Neal Lathia <neallathia@gmail.com>
2022-05-25 00:11:04 +0200

    Remove unused function

--- modelstore/metadata/code/dependencies.py
+++ modelstore/metadata/code/dependencies.py
@@ -68,11 +68,6 @@ def module_exists(modname: str) -> bool:
 
 def save_dependencies(tmp_dir: str, deps: list) -> str:
     """ Saves all of the current dependencies to file """
-    deps_info = get_dependency_versions(deps)
+    deps_info = get_dependency_versions(deps) 
     deps_info = {k: v for k, v in deps_info.items() if v is not None}
     return save_json(tmp_dir, _PYTHON_INFO_FILE, deps_info)
-
-
-# def save_model_info(tmp_dir, model_info: dict) -> str:
-#     """ Saves all of the meta-data about a model to file"""
-#     return save_json(tmp_dir, _MODEL_TYPE_FILE, model_info)
