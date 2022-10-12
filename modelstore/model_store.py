8c9f9e2c47df9b4625ab7fd7a24fab7ce76f7779
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-12 09:19:09 +0000

    Adding tarfile member sanitization to extractall()

--- modelstore/model_store.py
+++ modelstore/model_store.py
@@ -264,7 +264,26 @@ class ModelStore:
         local_path = os.path.abspath(local_path)
         archive_path = self.storage.download(local_path, domain, model_id)
         with tarfile.open(archive_path, "r:gz") as tar:
-            tar.extractall(local_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, local_path)
         os.remove(archive_path)
         return local_path
 
