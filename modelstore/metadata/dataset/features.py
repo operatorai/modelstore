be64c334eda609633bb780b9e78f662e92f27407
Neal Lathia <neallathia@gmail.com>
2023-03-19 19:32:16 +0000

    Set optional meta data fields to Optional[]

--- modelstore/metadata/dataset/features.py
+++ modelstore/metadata/dataset/features.py
@@ -11,7 +11,7 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
-from typing import Any, List
+from typing import Any, List, Optional
 
 from dataclasses import dataclass, field
 from dataclasses_json import dataclass_json
@@ -29,7 +29,7 @@ class Features:
     """Features contains fields that are captured about
     the training dataset's features when the model is saved"""
 
-    shape: List[int] = field(default=None, metadata=config(exclude=exclude_field))
+    shape: Optional[List[int]] = field(default=None, metadata=config(exclude=exclude_field))
 
     @classmethod
     def generate(cls, values: Any = None) -> "Features":
