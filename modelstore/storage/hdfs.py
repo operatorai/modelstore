bd1a24bbf6949c58263ae421090eed7b1d56f097
nlathia <neal.lathia@gmail.com>
2023-07-30 21:52:29 +0100

    Fix transformers logging bug

--- modelstore/storage/hdfs.py
+++ modelstore/storage/hdfs.py
@@ -114,7 +114,7 @@ class HdfsStorage(BlobStorage):
             if not hdfs.path.basename(obj).endswith(".json"):
                 logger.debug("Skipping non-json file: %s", obj)
                 continue
-            parent = obj[obj.index(prefix) :]
+            parent = obj[obj.index(prefix):]
             if os.path.split(parent)[0] != prefix:
                 # We don't want to read files in a sub-prefix
                 logger.debug("Skipping file in sub-prefix: %s", obj)
