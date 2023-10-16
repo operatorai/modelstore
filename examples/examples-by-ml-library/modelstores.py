e9c63a6fb8e341823a26cbeac95b3550b85098ce
nlathia <neal.lathia@gmail.com>
2023-10-16 22:43:52 +0100

    Disable hdfs tests in examples

--- examples/examples-by-ml-library/modelstores.py
+++ examples/examples-by-ml-library/modelstores.py
@@ -17,7 +17,7 @@ from modelstore import ModelStore
 from modelstore.storage.aws import AWSStorage
 from modelstore.storage.azure import AzureBlobStorage
 from modelstore.storage.gcloud import GoogleCloudStorage
-from modelstore.storage.hdfs import HdfsStorage
+# from modelstore.storage.hdfs import HdfsStorage
 from modelstore.storage.local import FileSystemStorage
 from modelstore.storage.minio import MinIOStorage
 
@@ -64,12 +64,12 @@ def create_gcloud_model_store() -> ModelStore:
     )
 
 
-def create_hdfs_model_store() -> ModelStore:
-    """A model store in HDFS storage"""
-    return ModelStore.from_hdfs(
-        root_prefix=os.environ["MODEL_STORE_HDFS_ROOT_PREFIX"],
-        create_directory=True,
-    )
+# def create_hdfs_model_store() -> ModelStore:
+#     """A model store in HDFS storage"""
+#     return ModelStore.from_hdfs(
+#         root_prefix=os.environ["MODEL_STORE_HDFS_ROOT_PREFIX"],
+#         create_directory=True,
+#     )
 
 
 def create_minio_model_store() -> ModelStore:
@@ -87,7 +87,7 @@ MODELSTORES = {
     AzureBlobStorage.NAME: create_azure_model_store,
     FileSystemStorage.NAME: create_file_system_model_store,
     GoogleCloudStorage.NAME: create_gcloud_model_store,
-    HdfsStorage.NAME: create_hdfs_model_store,
+    # HdfsStorage.NAME: create_hdfs_model_store,
     MinIOStorage.NAME: create_minio_model_store,
 }
 
