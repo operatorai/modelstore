e527382867294f93b2f3a88b3c49ea1c88882d69
Ibrahim <ibrahim.m.faruqi@gmail.com>
2024-05-01 17:33:52 +0100

    Remove support for mxnet

--- examples/examples-by-ml-library/run-all.sh
+++ examples/examples-by-ml-library/run-all.sh
@@ -1,7 +1,7 @@
 set -e
 backends=( filesystem aws-s3 google-cloud-storage azure-container minio )
 frameworks=( annoy catboost fastai file gensim keras lightgbm \
-	mxnet onnx-sklearn onnx-lightgbm prophet pyspark pytorch pytorch-lightning \
+	onnx-sklearn onnx-lightgbm prophet pyspark pytorch pytorch-lightning \
 	sklearn sklearn-with-explainer sklearn-with-extras skorch xgboost xgboost-booster \
 	tensorflow hf-distilbert hf-gpt2-pt hf-gpt2-tf segment-anything yolov5 )
 
